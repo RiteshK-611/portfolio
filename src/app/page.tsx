6e5304ccc6bd9be659ffb7c9ac3088626b4c1a76
Ritesh <riteshkokam@gmail.com>
2024-06-10 22:40:56 +0530

    feat: Add Project component and update Projects page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
+import Project from "@/components/Project";
 import Projects from "@/components/Projects";
 import Image from "next/image";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
+      <Project/>
       <Projects />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { PinContainer } from "./ui/3d-pin";
 import Image from "next/image";
+import { Dock, DockIcon } from "./ui/dock";
+import { Icons } from "@/lib/Icons";
 
 const Projects = () => {
   return (
@@ -11,8 +13,7 @@ const Projects = () => {
         title="/ui.aceternity.com"
         href="https://twitter.com/mannupaaji">
         <div className="relative flex basis-full p-1 tracking-tight text-slate-100/50 sm:basis-1/2 w-[30rem] h-[20rem]">
-          <div
-            className="relative overflow-hidden lg:rounded-xl flex justify-center bg-[#13162D]">
+          <div className="relative overflow-hidden lg:rounded-xl flex justify-center text-card-foreground shadow bg-primary/5">
             <Image
               src="/inar.jpg"
               alt="cover"
@@ -24,27 +25,44 @@ const Projects = () => {
           </div>
         </div>
       </PinContainer>
-      <div className="col-span-3 m-auto pt-5 md:pt-0 p-1 ">
-        <p className="pb-4 text-xl font-bold group-hover:underline-offset-4 group-hover:underline ease-in duration-300">
-          Project
+      <div className="col-span-3 mt-4 pt-5 md:pt-0 rounded-xl p-2 ring-1 ring-inset ring-primary/10 lg:mx-2 lg:-mb-8 md:my-0 lg:rounded-2xl lg:p-5">
+        <p className="pb-4 text-3xl font-bold group-hover:underline-offset-4 group-hover:underline ease-in duration-300">
+          InAR
         </p>
-        <p>My project description</p>
-        <div className="pt-3">
-          <p className="text-sm font-semibold pb-1">Technologies used:</p>
-          {/* {usedArray.map((used, i) => {
-            return (
-              <span key={i} className="rounded-[3px] text-xs font-bold text-[#0967d2] border border-[#0967d2] border-sky-500 m-[3px] first-of-type:ml-0 py-1 px-3 hover:bg-blue-500 hover:text-white inline-block ease-in duration-300">
-                {used}
-              </span>
-            );
-          })} */}
+        <div className="rounded-xl border text-card-foreground shadow bg-primary/5 p-2 lg:p-5 h-[82%]">
+          <p>
+            In Augmented Reality (InAR). It is an Augmented Reality (AR) based
+            Ecommerce application. It uses Web-based AR or WebAR to place
+            furniture/product in your environment, to get clear knowledge of
+            whether this product fits in our environment before buying it out.
+          </p>
+          <div className="pt-3">
+            <p className="text-sm font-mono">Tech Stack</p>
+            <Dock className="left-0">
+              <DockIcon>
+                <Icons.gitHub className="h-6 w-6" />
+              </DockIcon>
+              <DockIcon>
+                <Icons.googleDrive className="h-6 w-6" />
+              </DockIcon>
+              <DockIcon>
+                <Icons.notion className="h-6 w-6" />
+              </DockIcon>
+              <DockIcon>
+                <Icons.openai className="h-6 w-6" />
+              </DockIcon>
+              <DockIcon>
+                <Icons.whatsapp className="h-6 w-6" />
+              </DockIcon>
+            </Dock>
+          </div>
+          <Link href="#">
+            <button className="px-5 py-2 mt-4 mr-8">Demo</button>
+          </Link>
+          <Link href="#">
+            <button className="px-5 py-2 mt-4">Code</button>
+          </Link>
         </div>
-        <Link href="#">
-          <button className="px-5 py-2 mt-4 mr-8">Demo</button>
-        </Link>
-        <Link href="#">
-          <button className="px-5 py-2 mt-4">Code</button>
-        </Link>
       </div>
     </div>
   );
