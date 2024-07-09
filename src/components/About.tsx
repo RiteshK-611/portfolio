6bb7a7332bf0ddc1c094226d059be1823fb19753
Ritesh <riteshkokam@gmail.com>
2024-07-09 07:22:08 +0530

    Update about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import pic from "../../public/profile.png";
 import { motion } from "framer-motion";
 import IconCloud from "./ui/icon-cloud";
+import Globe from "./ui/globe";
 
 const slugs = [
   "typescript",
@@ -51,8 +52,16 @@ const About = () => {
         </p>
         <h2 className="py-4 dark:text-white">Who I Am</h2>
 
-        <div className="grid grid-cols-5 gap-8 max-w-7xl w-full mx-auto p-4 rounded-md my-12 grid-flow-dense">
-          <div className="rounded-md border relative min-h-36 grid-flow-dense col-span-3 row-span-2"></div>
+        <div className="grid grid-cols-5 gap-8 max-w-7xl w-full mx-auto p-4 rounded-md my-12 grid-flow-dense overflow-hidden">
+          <div className="rounded-md border relative min-h-36 grid-flow-dense col-span-3 row-span-2">
+            <div className="relative h-full w-full min-w-[32rem] overflow-hidden rounded-lg border bg-background flex items-center pt-8 md:pb-60 md:shadow-xl">
+              <span className="z-10 pointer-events-none whitespace-pre-wrap text-center text-4xl font-semibold leading-none">
+                I&apos;m very flexible with timezone communications
+              </span>
+              <Globe className="top-16" />
+              <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+            </div>
+          </div>
           <div className="rounded-md border relative min-h-36 grid-flow-dense col-span-2 row-span-2">
             <Canva />
           </div>
