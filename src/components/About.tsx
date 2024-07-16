d42abf68849028215e8367e7a2bc49b460ce6daf
Ritesh <riteshkokam@gmail.com>
2024-07-17 00:24:52 +0530

    Refactor About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -51,42 +51,9 @@ const About = () => {
         <h2 className="py-4 dark:text-white">Who I Am</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-5 gap-4 md:gap-8 max-w-7xl w-full mx-auto p-4 rounded-md my-12 grid-flow-dense overflow-hidden">
-          <div className="rounded-xl ring-1 ring-inset ring-ring relative min-h-36 grid-flow-dense md:col-span-2 md:row-span-1 col-span-1 row-span-1 flex items-center overflow-hidden">
-            <span className="z-10 pointer-events-none whitespace-pre-wrap text-xl md:text-3xl font-semibold leading-none px-10 mb-5">
-              Tech enthusiast with a passion for development.
-            </span>
-            <DotPattern
-              className={cn(
-                "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] left-[2%] top-[4%]"
-              )}
-            />
-            <div className="absolute right-0 -bottom-5">
-              {theme === "dark" ? (
-                <Image
-                  src={"/assets/svgs/bgd.svg"}
-                  width="200"
-                  height="200"
-                  alt=""
-                />
-              ) : (
-                <Image
-                  src={"/assets/svgs/bgl.svg"}
-                  width="200"
-                  height="200"
-                  alt=""
-                />
-              )}
-              <Image
-                src={"/assets/svgs/bgd.svg"}
-                width="200"
-                height="200"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="rounded-xl ring-1 ring-inset ring-ring relative min-h-36 grid-flow-dense md:col-span-2 md:col-start-2 md:row-span-1 col-span-1 row-span-1">
-            <div className="relative h-full w-full min-w-full overflow-hidden rounded-lg border bg-background flex items-center pt-8 pb-24 md:shadow-xl">
-              <span className="z-10 pointer-events-none whitespace-pre-wrap text-center text-xl md:text-3xl px-5 font-semibold leading-none">
+          <div className="rounded-xl ring-1 ring-inset ring-ring relative min-h-36 grid-flow-dense col-span-3 row-span-2">
+            <div className="relative h-full w-full min-w-[32rem] overflow-hidden rounded-lg border bg-background flex items-center pt-8 md:pb-60 md:shadow-xl">
+              <span className="z-10 pointer-events-none whitespace-pre-wrap text-center text-3xl px-5 font-semibold leading-none">
                 I&apos;m very flexible with timezone communications
               </span>
               <Globe className="top-16" />
@@ -129,12 +96,6 @@ const About = () => {
                   alt=""
                 />
               )}
-              <Image
-                src={"/assets/svgs/bgd.svg"}
-                width="200"
-                height="200"
-                alt=""
-              />
             </div>
           </div>
           <div className="rounded-xl ring-1 ring-inset ring-ring relative min-h-36 grid-flow-dense md:col-span-2 md:col-start-4 md:row-span-1 col-span-1 row-span-1 flex flex-col items-center justify-evenly">
@@ -142,9 +103,9 @@ const About = () => {
             <span className="z-10 pointer-events-none whitespace-pre-wrap text-center text-xl md:text-3xl font-semibold leading-none px-10">
               Do you want to start a project together?
             </span>
-            <button className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <button className="relative inline-flex h-14 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-background px-8 py-1 text-sm font-medium text-foreground backdrop-blur-3xl">
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-background px-8 py-2 text-lg font-medium text-foreground backdrop-blur-3xl">
                 Let&apos;s Connect
               </span>
             </button>
