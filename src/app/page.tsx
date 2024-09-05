625a6d3dec8988b4f8a81a66f0f8ac1fd3333572
Ritesh <riteshkokam@gmail.com>
2024-09-05 23:58:06 +0530

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <div className="bg-gradient-to-b from-[#ff520e] via-[#ff520e] to-[#ff520e] blur-[100px] flex-none h-[212px] opacity-50 overflow-visible absolute right-0 top-0 w-[1047px] z-[1]"></div>
       <Navbar />
       <Hero />
       <About />
